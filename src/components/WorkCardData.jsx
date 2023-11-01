706648baa4d63e87ebce710a924608c555c71a9b
Raviteja <ravitejasiriki08@gmail.com>
2023-11-01 12:36:19 +0530

    committed

---
+++ src/components/WorkCardData.jsx
@@ -0,0 +1,24 @@
+import pro1 from '../assests/project1.png'
+import pro2 from '../assests/project2.png'
+import pro3 from '../assests/project3.png'
+const ProjectCardData=[
+    {
+        imgsrc:pro1,
+        title:"Ecommerce Website Desgin",
+        text:"Text need to add using chat gpt",
+        view:"need to add url of the shopping ui"
+    },
+    {
+        imgsrc:pro2,
+        title:"Ecommerce Website Desgin",
+        text:"Text need to add using chat gpt",
+        view:"need to add url of the shopping ui"
+    },
+    {
+        imgsrc:pro3,
+        title:"Ecommerce Website Desgin",
+        text:"Text need to add using chat gpt",
+        view:"need to add url of the shopping ui"
+    }
+];
+export default ProjectCardData;
